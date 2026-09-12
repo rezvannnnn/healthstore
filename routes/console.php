c17e03b5556a4e0e623ffa0f461b7ec7d7580d71
rezvannnnn <rezvan.ehsan@gmail.com>
2026-09-12 17:46:03 +0330

    Add scheduled expired reservation cleanup

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Services\InventoryReservationService;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('inventory:release-expired-reservations', function (InventoryReservationService $service) {
+    $releasedCount = $service->releaseExpired();
+
+    $this->info("Released {$releasedCount} expired inventory reservation(s).");
+})->purpose('Release expired inventory reservations');
