eb7b2b7666caae58b48fcee4458621668060dff2
rezvannnnn <rezvan.ehsan@gmail.com>
2026-09-12 20:26:55 +0330

    Add admin product create and edit routes

--- routes/web.php
+++ routes/web.php
@@ -71,6 +71,10 @@ Route::middleware(['auth', 'admin'])
     ->group(function () {
         Route::get('/', DashboardController::class)->name('dashboard');
         Route::get('/products', [AdminProductController::class, 'index'])->name('products.index');
+        Route::get('/products/create', [AdminProductController::class, 'create'])->name('products.create');
+        Route::post('/products', [AdminProductController::class, 'store'])->name('products.store');
+        Route::get('/products/{product}/edit', [AdminProductController::class, 'edit'])->name('products.edit');
+        Route::put('/products/{product}', [AdminProductController::class, 'update'])->name('products.update');
     });
 
 Route::middleware('auth')->group(function () {
