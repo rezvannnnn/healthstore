6f27296e34e4f7f9118b17a52981c96d40a4c58f
rezvannnnn <rezvan.ehsan@gmail.com>
2026-09-13 20:01:37 +0330

    Add article model

---
+++ app/Models/Article.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Article extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'category_id',
+        'author_id',
+        'title',
+        'slug',
+        'excerpt',
+        'content',
+        'featured_image',
+        'featured_image_alt',
+        'seo_title',
+        'seo_description',
+        'canonical_url',
+        'is_active',
+        'is_featured',
+        'published_at',
+    ];
+
+    protected $casts = [
+        'is_active' => 'boolean',
+        'is_featured' => 'boolean',
+        'published_at' => 'datetime',
+    ];
+
+    /**
+     * @return BelongsTo<ArticleCategory, $this>
+     */
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(ArticleCategory::class, 'category_id');
+    }
+
+    /**
+     * @return BelongsTo<User, $this>
+     */
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'author_id');
+    }
+}
