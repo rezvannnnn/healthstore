8a0afe7a3d6623698aa22efb3b033d1f6c7fc516
rezvannnnn <rezvan.ehsan@gmail.com>
2026-09-13 20:02:48 +0330

    Add admin article create page

---
+++ resources/js/pages/Admin/Articles/Create.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import Form from './Form.vue';
+
+defineProps<{ categories: { id: number; name: string }[] }>();
+</script>
+
+<template><Form :categories="categories" /></template>
