6a900e55fb08f774f90a90b536413323fd69c4e3
rezvannnnn <rezvan.ehsan@gmail.com>
2026-09-14 15:19:56 +0330

    Test public XML sitemap endpoint

---
+++ tests/Feature/SitemapTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class SitemapTest extends TestCase
+{
+    public function test_sitemap_is_public_xml(): void
+    {
+        $response = $this->get('/sitemap.xml');
+
+        $response->assertOk()
+            ->assertHeader('content-type', 'application/xml; charset=UTF-8')
+            ->assertSee('<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">', false)
+            ->assertSee(route('home'), false)
+            ->assertSee(route('products.index'), false)
+            ->assertSee(route('blog.index'), false);
+    }
+}
