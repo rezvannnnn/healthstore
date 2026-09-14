9c84798263c14ea9792daf68fc56a1b105a17781
rezvannnnn <rezvan.ehsan@gmail.com>
2026-09-15 00:38:10 +0330

    feat: share status flash messages with Inertia

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -45,6 +45,7 @@ class HandleInertiaRequests extends Middleware
                 'success' => fn () => $request->session()->get('success'),
                 'error' => fn () => $request->session()->get('error'),
                 'info' => fn () => $request->session()->get('info'),
+                'status' => fn () => $request->session()->get('status'),
             ],
         ];
     }
