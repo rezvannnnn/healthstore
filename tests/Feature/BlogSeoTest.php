f2ed79f6f6557bc2548ded487e9758983c279587
rezvannnnn <rezvan.ehsan@gmail.com>
2026-09-14 22:47:49 +0330

    test: prepare database for blog index SEO coverage

--- tests/Feature/BlogSeoTest.php
+++ tests/Feature/BlogSeoTest.php
@@ -2,10 +2,13 @@
 
 namespace Tests\Feature;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class BlogSeoTest extends TestCase
 {
+    use RefreshDatabase;
+
     public function test_blog_index_is_public_and_renders_successfully(): void
     {
         $response = $this->get('/blog');
