fd3dbefd60b06f128934bd664d2814cdb51c2648
rezvannnnn <rezvan.ehsan@gmail.com>
2026-09-14 22:50:47 +0330

    SEO: improve blog index title (#34)
    * test: add blog SEO regression coverage
    
    * seo: improve blog index title
    
    * fix: remove unavailable article factory from blog SEO test
    
    * test: prepare database for blog index SEO coverage
    
    * test: assert blog index response status

---
+++ tests/Feature/BlogSeoTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class BlogSeoTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_blog_index_is_public_and_renders_successfully(): void
+    {
+        $response = $this->get('/blog');
+
+        $response->assertOk();
+    }
+}
