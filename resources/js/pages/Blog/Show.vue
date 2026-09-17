b9f0be279f79775286d110fb36e257d06caae558
rezvannnnn <rezvan.ehsan@gmail.com>
2026-09-17 13:25:08 +0330

    Add Article structured data to blog pages

--- resources/js/pages/Blog/Show.vue
+++ resources/js/pages/Blog/Show.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { Head, Link } from '@inertiajs/vue3';
+import { computed } from 'vue';
 
 interface Article {
     title: string;
@@ -17,6 +18,21 @@ interface Article {
 }
 
 defineProps<{ article: Article }>();
+
+const props = defineProps<{ article: Article }>();
+const structuredData = computed(() => ({
+    '@context': 'https://schema.org',
+    '@type': 'Article',
+    headline: props.article.title,
+    description: props.article.seo_description || props.article.excerpt || undefined,
+    url: props.article.canonical_url || `/blog/${props.article.slug}`,
+    image: props.article.featured_image ? [props.article.featured_image] : undefined,
+    datePublished: props.article.published_at || undefined,
+    author: props.article.author
+        ? { '@type': 'Person', name: props.article.author.name }
+        : undefined,
+    articleSection: props.article.category?.name || undefined,
+}));
 </script>
 
 <template>
@@ -69,6 +85,7 @@ defineProps<{ article: Article }>();
             name="twitter:image"
             :content="article.featured_image"
         />
+        <script type="application/ld+json">{{ JSON.stringify(structuredData) }}</script>
     </Head>
     <div class="mx-auto max-w-4xl space-y-6 p-6" dir="rtl">
         <Link href="/blog" class="text-sm underline"
