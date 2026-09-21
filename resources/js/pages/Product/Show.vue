c6100ab2b8d823cabc498cb1a860d5275547eab7
rezvannnnn <rezvan.ehsan@gmail.com>
2026-09-21 14:38:42 +0330

    fix: remove unused product show import

--- resources/js/pages/Product/Show.vue
+++ resources/js/pages/Product/Show.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { Head, Link, router } from '@inertiajs/vue3';
+import { Link, router } from '@inertiajs/vue3';
 import { computed, ref } from 'vue';
 
 interface ProductImage {
