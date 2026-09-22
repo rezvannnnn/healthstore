d6f8237b086171a93654280425cff412cad0bbce
rezvannnnn <rezvan.ehsan@gmail.com>
2026-09-22 13:59:10 +0330

    Add reusable Darookhooneh logo component

---
+++ resources/js/components/BrandLogo.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+withDefaults(
+    defineProps<{
+        class?: string;
+        imageClass?: string;
+        label?: string;
+    }>(),
+    {
+        class: '',
+        imageClass: '',
+        label: 'داروخونه',
+    },
+);
+</script>
+
+<template>
+    <span :class="['inline-flex shrink-0 items-center', $props.class]">
+        <img
+            src="/images/darukhooneh-logo.webp"
+            :alt="label"
+            :class="['h-16 w-16 object-contain', imageClass]"
+        />
+    </span>
+</template>
