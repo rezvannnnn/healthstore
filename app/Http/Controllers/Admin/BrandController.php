81250b0d506a48a53615d176fba3323f606a1301
rezvannnnn <rezvan.ehsan@gmail.com>
2026-09-25 11:21:10 +0330

    Use Pint-compatible admin controller constructors

--- app/Http/Controllers/Admin/BrandController.php
+++ app/Http/Controllers/Admin/BrandController.php
@@ -14,7 +14,12 @@ use Inertia\Response;
 
 class BrandController extends Controller
 {
-    public function __construct(protected MediaService $mediaService) {}
+    protected MediaService $mediaService;
+
+    public function __construct(MediaService $mediaService)
+    {
+        $this->mediaService = $mediaService;
+    }
 
     public function index(): Response
     {
