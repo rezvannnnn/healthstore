bb5b3d553f5a3f01c026231550795e07e2e145d3
rezvannnnn <rezvan.ehsan@gmail.com>
2026-09-25 13:53:06 +0330

    Align brand controller imports with Pint

--- app/Http/Controllers/Admin/BrandController.php
+++ app/Http/Controllers/Admin/BrandController.php
@@ -9,9 +9,9 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use Illuminate\Validation\Rule;
-use Throwable;
 use Inertia\Inertia;
 use Inertia\Response;
+use Throwable;
 
 class BrandController extends Controller
 {
