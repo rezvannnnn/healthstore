4b110b17092c0242eb0c3047bdf238951bab075a
rezvannnnn <rezvan.ehsan@gmail.com>
2026-09-25 11:21:07 +0330

    Use Pint-compatible admin controller constructors

--- app/Http/Controllers/Admin/ArticleController.php
+++ app/Http/Controllers/Admin/ArticleController.php
@@ -15,7 +15,12 @@ use Inertia\Response;
 
 class ArticleController extends Controller
 {
-    public function __construct(protected MediaService $mediaService) {}
+    protected MediaService $mediaService;
+
+    public function __construct(MediaService $mediaService)
+    {
+        $this->mediaService = $mediaService;
+    }
 
     public function index(Request $request): Response
     {
