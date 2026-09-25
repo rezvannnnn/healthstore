8772db6d77abbfb57ffc42c158ea76f7c99c38bd
rezvannnnn <rezvan.ehsan@gmail.com>
2026-09-25 11:19:42 +0330

    Stabilize Pint formatting for admin controllers

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -15,9 +15,7 @@ use Inertia\Response;
 
 class CategoryController extends Controller
 {
-    public function __construct(
-        protected MediaService $mediaService,
-    ) {}
+    public function __construct(protected MediaService $mediaService) {}
 
     public function index(): Response
     {
