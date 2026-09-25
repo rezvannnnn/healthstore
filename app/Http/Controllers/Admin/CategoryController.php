06b7ef7bba0c4264d4fb8345275df5c367db0054
rezvannnnn <rezvan.ehsan@gmail.com>
2026-09-25 11:23:54 +0330

    Correct Pint formatting changes safely

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -1,27 +1,20 @@
-    protected MediaService $mediaService;
-
-    public function __construct(MediaService $mediaService)
-    {
-        $this->mediaService = $mediaService;
-    }<?php
+<?php
 
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Category;
+use App\Services\MediaService;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
-use App\Services\MediaService;
 use Illuminate\Validation\Rule;
 use Inertia\Inertia;
 use Inertia\Response;
 
 class CategoryController extends Controller
 {
-    protected MediaService $mediaService;
-
     public function __construct(MediaService $mediaService)
     {
         $this->mediaService = $mediaService;
