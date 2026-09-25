45f025e48d1985a89a496378094391e0dc5dc66c
rezvannnnn <rezvan.ehsan@gmail.com>
2026-09-25 11:24:22 +0330

    Finalize Pint-compatible MediaService constructors

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -15,10 +15,7 @@ use Inertia\Response;
 
 class CategoryController extends Controller
 {
-    public function __construct(MediaService $mediaService)
-    {
-        $this->mediaService = $mediaService;
-    }
+    public function __construct(protected MediaService $mediaService) {}
 
     public function index(): Response
     {
